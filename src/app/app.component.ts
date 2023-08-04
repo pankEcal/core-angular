35fc2769a070996aca7d6b84f58eef9b6986672c
pankecal <pankaj.thapaliya@enginecal.com>
2023-08-04 12:12:11 +0530

    implement standard component implementation with separate html and css files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>My App Entry</h1>`,
-  styles: [
-    `
-      h1 {
-        background-color: red;
-      }
-    `,
-  ],
+  selector: 'my-app-main',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
